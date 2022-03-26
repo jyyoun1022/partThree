2fcac75a3feebf8bd829142642d8aab2f3038710
jyyoun1022 <jyyoun1022@naver.com>
2022-03-26 18:15:09 +0900

    Initial commit

---
+++ src/main/java/org/zerock/partThree/PartThreeApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.partThree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartThreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zerock/partThree/PartThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.partThree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
